c82edc1a6dbd5ce10c827c121aff0299ed448343
Kaden Adlington <kad112@uclive.ac.nz>
2024-10-15 17:31:12 +1300

    Modified selectAttack() to include the code responsible for firing the IR sensor to the other unit, and waiting for a response.

--- attack.c
+++ attack.c
@@ -5,7 +5,6 @@
 #include "ir_uart.h"
 #include "tinygl.h"
 #include "pacer.h"
-#include "../fonts/font5x7_1.h"
 #include <stdio.h>
 
 
@@ -15,34 +14,7 @@ char received_char = ' ';
 
 void attack_phase(void)
 {
-    while (1) {
-        button_update();
-        if (button_push_event_p(0)) {
-            // Broadcast the coordinate until a character is received
-            while (1) {
-                // Check if a character is ready to be received
-                if (ir_uart_read_ready_p()) {
-                    // Extract the received character
-                    received_char = ir_uart_getc(); 
-                    // Check if the received character is '-' or '+'
-                    if (received_char == '-' || received_char == '+') {
-                        break; // Exit the loop if '-' or '+' is received
-                    }
-                }
-                // Send the coordinate if the received character is not '-' or '+'
-                send_coordinate(1, 4);
-            }
-            
-            if (received_char == '+') {
-                led_set(LED1, 1);
-            } else {
-                led_set(LED1, 0);
-            }
-            break; // Exit the loop once a character is received and processed
-        } else {
-            break;
-        }
-    }
+    selectAttack();
 }
 
 // Function to encode coordinates (x, y) into a single ASCII character
@@ -75,7 +47,27 @@ void selectAttack()
     attack_t attack = {.col = startPosition.x, .row = startPosition.y};
     tinygl_draw_point(tinygl_point(attack.col, attack.row), 1);
     while(1) {
-        tinygl_update();
-        pacer_wait();
+        // Broadcast the coordinate until a character is received
+        while (1) {
+            // Check if a character is ready to be received
+            if (ir_uart_read_ready_p()) {
+                // Extract the received character
+                received_char = ir_uart_getc(); 
+                // Check if the received character is '-' or '+'
+                if (received_char == '-' || received_char == '+') {
+                    break; // Exit the loop if '-' or '+' is received
+                }
+            }
+            // Send the coordinate if the received character is not '-' or '+'
+            send_coordinate(attack.col, attack.row);
+        }
+        
+        // Is it a hit or a miss?
+        if (received_char == '+') {
+            led_set(LED1, 1);
+        } else {
+            led_set(LED1, 0);
+        }
+        break; // Exit the loop once a character is received and processed
     }
 }
